47e52f1ddfeb1c756e0cf2277006a72fe4524ec2
Victoria Brekenfeld <4404502+Drakulix@users.noreply.github.com>
2023-01-16 18:09:11 +0100

    iced: Use iced_native for keyboard types (#25)

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,3 +1,2 @@
 //! Listen and react to keyboard events.
-#[cfg(any(feature = "winit", feature = "wayland"))]
-pub use crate::runtime::keyboard::{Event, KeyCode, Modifiers};
+pub use iced_native::keyboard::{Event, KeyCode, Modifiers};
