483f3a10411d9830223ed469a806498f2677c134
Héctor Ramón Jiménez <hector0193@gmail.com>
2023-06-27 19:41:03 +0200

    Move `Screenshot` inside `window` module

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -34,7 +34,6 @@ mod debug;
 #[cfg(not(feature = "debug"))]
 #[path = "debug/null.rs"]
 mod debug;
-mod screenshot;
 
 pub use iced_core as core;
 pub use iced_futures as futures;
@@ -43,5 +42,4 @@ pub use command::Command;
 pub use debug::Debug;
 pub use font::Font;
 pub use program::Program;
-pub use screenshot::{CropError, Screenshot};
 pub use user_interface::UserInterface;
