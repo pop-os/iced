384945a53463627ae0b274631d5da1f85ac97f0b
Victoria Brekenfeld <victoria@system76.com>
2023-11-01 20:36:42 +0100

    sctk: Implement xdg-activation support

--- runtime/src/command/platform_specific/wayland/mod.rs
+++ runtime/src/command/platform_specific/wayland/mod.rs
@@ -4,6 +4,8 @@ use std::fmt::Debug;
 
 use iced_futures::MaybeSend;
 
+/// activation Actions
+pub mod activation;
 /// data device Actions
 pub mod data_device;
 /// layer surface actions
@@ -23,6 +25,8 @@ pub enum Action<T> {
     Popup(popup::Action<T>),
     /// data device
     DataDevice(data_device::Action<T>),
+    /// activation
+    Activation(activation::Action<T>),
 }
 
 impl<T> Action<T> {
@@ -40,6 +44,7 @@ impl<T> Action<T> {
             Action::Window(a) => Action::Window(a.map(f)),
             Action::Popup(a) => Action::Popup(a.map(f)),
             Action::DataDevice(a) => Action::DataDevice(a.map(f)),
+            Action::Activation(a) => Action::Activation(a.map(f)),
         }
     }
 }
@@ -54,6 +59,9 @@ impl<T> Debug for Action<T> {
             Self::Popup(arg0) => f.debug_tuple("Popup").field(arg0).finish(),
             Self::DataDevice(arg0) => {
                 f.debug_tuple("DataDevice").field(arg0).finish()
+            }
+            Self::Activation(arg0) => {
+                f.debug_tuple("Activation").field(arg0).finish()
             }
         }
     }
