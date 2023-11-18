afff0c170188e4eaa49aeea5c043304fca878a6c
Ashley Wulber <ashley@system76.com>
2023-11-17 19:44:47 -0500

    fix: don't require Flags to be clone for settings on wayland

--- sctk/src/application.rs
+++ sctk/src/application.rs
@@ -222,12 +222,10 @@ where
     E: Executor + 'static,
     C: Compositor<Renderer = A::Renderer> + 'static,
     <A::Renderer as Renderer>::Theme: StyleSheet,
-    A::Flags: Clone,
 {
     let mut debug = Debug::new();
     debug.startup_started();
 
-    let flags = settings.flags.clone();
     let exit_on_close_request = settings.exit_on_close_request;
 
     let mut event_loop = SctkEventLoop::<A::Message>::new(&settings)
@@ -241,7 +239,7 @@ where
     };
 
     let (application, init_command) = {
-        let flags = flags;
+        let flags = settings.flags;
 
         runtime.enter(|| A::new(flags))
     };
