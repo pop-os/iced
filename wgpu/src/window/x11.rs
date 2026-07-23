5d19e9a9277170fe4e4e4706c2e9b79add9c3386
sanua356 <sanek.pankratov356@gmail.com>
2026-07-23 21:12:04 +0300

    Update x11.rs

--- wgpu/src/window/x11.rs
+++ wgpu/src/window/x11.rs
@@ -146,17 +146,9 @@ pub fn get_x11_device_ids<W: Window>(window: &W) -> Option<(u16, u16)> {
                                     {
                                         let stat =
                                             stat(Path::new("/dev/dri").join(device)).ok()?;
-                                        let dev: u64 = {
-                                            #[cfg(not(target_arch = "e2k"))]
-                                            {
-                                                stat.st_rdev
-                                            }
-
-                                            #[cfg(target_arch = "e2k")]
-                                            {
-                                                stat.st_rdev as u64
-                                            }
-                                        };
+                                        // Hack (u64 as u64) for E2K CPU architecture
+                                        #[allow(clippy::unnecessary_cast)]
+                                        let dev = stat.st_rdev as u64;
                                         return super::ids_from_dev(dev);
                                     }
                                 }
@@ -177,17 +169,9 @@ pub fn get_x11_device_ids<W: Window>(window: &W) -> Option<(u16, u16)> {
         let dri3 = conn.dri3_open(root, x11rb::NONE).ok()?.reply().ok()?;
         let device_fd = dri3.device_fd;
         let stat = fstat(device_fd).ok()?;
-        let dev: u64 = {
-            #[cfg(not(target_arch = "e2k"))]
-            {
-                stat.st_rdev
-            }
-
-            #[cfg(target_arch = "e2k")]
-            {
-                stat.st_rdev as u64
-            }
-        };
+        // Hack (u64 as u64) for E2K CPU architecture
+        #[allow(clippy::unnecessary_cast)]
+        let dev = stat.st_rdev as u64;
         super::ids_from_dev(dev)
     }
 }
